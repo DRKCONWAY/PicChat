592d553d744f2dce3e921f716cf903b7526787d9
Derek Conway <DRKCONWAY@GMAIL.COM>
2016-10-17 15:32:22 -0500

    Setting up

--- PicChat/CameraVC.swift
+++ PicChat/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  PicChat
 //
 //  Created by D on 10/17/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: AAPLCameraViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
